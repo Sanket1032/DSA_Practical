dd0e607c3282ad06dd66db42e07432b19281ba68
Sanket1032 <sanketshinde1032@gmail.com>
2022-03-08 22:49:55 +0530

    searching and sorting

---
+++ Searching/Binary.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+int binary(int arr[],int n,int key){
+    int s=0;
+    int e=n-1;
+    int mid=(s+e)/2;
+    while(s<=e){
+        if(arr[mid]==key){
+            return mid;
+        }
+        else if(arr[mid]>key){
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int key;
+    cin>>key;
+    cout<<binary(arr,n,key);
+    return 0;
+}
