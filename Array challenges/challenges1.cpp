242b881a639ac649ec9fd2feacd367e244e6487c
Sanket1032 <sanketshinde1032@gmail.com>
2022-03-15 14:56:52 +0530

    array challenges

--- Array challenges/challenges1.cpp
+++ Array challenges/challenges1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <climits>
+#include <algorithm>
 using namespace std;
 
 // int main()
@@ -23,25 +24,96 @@ using namespace std;
 // }
 
 // Adding all the subarrays
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         int sum = 0;
+//         for (int j = i; j < n; j++)
+//         {
+//             sum += arr[j];
+//             cout << sum << endl;
+//         }
+//     }
+
+//     return 0;
+// }
+
+// Finding the max arthmatic array
+// int main()
+// {
+//     int test;
+//     cin >> test;
+//     while (test--)
+//     {
+//         int n;
+//         cin >> n;
+//         int arr[n];
+//         for (int i = 0; i < n; i++)
+//         {
+//             cin >> arr[i];
+//         }
+//         int pd = arr[1] - arr[0];
+//         int curr = 2;
+//         int ans = 2;
+//         int j = 2;
+//         while (j < n)
+//         {
+//             if (pd == arr[j] - arr[j - 1])
+//             {
+//                 curr++;
+//             }
+//             else
+//             {
+//                 pd = arr[j] - arr[j - 1];
+//                 curr = 2;
+//             }
+//             ans = max(ans, curr);
+//             j++;
+//         }
+//         cout << ans << endl;
+//     }
+//     return 0;
+// }
+
+// Record breaker
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    int test;
+    cin >> test;
+    while (test--)
     {
-        cin >> arr[i];
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        int sum = 0;
-        for (int j = i; j < n; j++)
+        int n;
+        cin >> n;
+        int arr[n + 1];
+        arr[n] = -1;
+        for (int i = 0; i < n; i++)
         {
-            sum += arr[j];
-            cout << sum << endl;
+            cin >> arr[i];
         }
+        if (n == 1)
+        {
+            cout << "1" << endl;
+        }
+        int ans = 0;
+        int mx = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > mx)
+            {
+                ans++;
+            }
+            mx = max(arr[i], mx);
+        }
+        cout << ans << endl;
     }
-
     return 0;
 }
